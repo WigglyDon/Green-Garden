288fc3af79218a92a329231e977e387335a2d677
Edmund <eyu1@ualberta.ca>
2021-11-21 20:16:10 -0700

    update readme todo list

--- back-end-express/db/seeds/05_notifications.sql
+++ back-end-express/db/seeds/05_notifications.sql
@@ -1,7 +1,7 @@
 INSERT INTO notifications (garden_id, day, hour, minute, body)
 VALUES
-(1,[6],12,55,'this means saturday, 4:25 pm for garden id 1'),
-(1,[6],16,25,'this means saturday, 4:25 pm for garden id 1')
+(1,6,12,55,'this means saturday, 4:25 pm for garden id 1'),
+(1,6,16,25,'this means saturday, 4:25 pm for garden id 1')
 
 
 
