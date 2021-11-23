a6c693e2bf6f2bcdfac68e94df9cda42f2095965
Edmund <eyu1@ualberta.ca>
2021-11-22 17:51:54 -0700

    Add react router routes for /, dashboard, and login

--- front-end-react/src/components/Application.tsx
+++ front-end-react/src/components/Application.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import Scheduler from "./Scheduler /Scheduler";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import AdapterDateFns from "@mui/lab/AdapterDateFns";
+// import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import useApplicationData from "../hooks/useApplicationData";
 import Nav from "./Nav";
 import SearchBar from "./SearchBar";
@@ -10,22 +9,24 @@ import VegetableCardList from "./VegetableCardList";
 import GardenCardList from "./GardenCardList";
 import Dashboard from "./Dashboard/Dashboard";
 import SideBarList from "./SideBar/SideBarList";
+import { Link } from "react-router-dom";
 
 export default function Application() {
   const { state, handleDayChange, handleTime, bookNotification } =
     useApplicationData();
   return (
     <div>
+      <Link to="/dashboard">My Garden</Link> | <Link to="/login">Login</Link>
       <Nav />
       <SearchBar />
       <VegetableCardList />
       <GardenCardList />
-      <Dashboard
+      {/* <Dashboard
         state={state}
         handleDayChange={handleDayChange}
         handleTime={handleTime}
         bookNotification={bookNotification}
-      />
+      /> */}
       <SideBarList />
     </div>
   );
---
+++ front-end-react/src/routes/dashboard.tsx
@@ -0,0 +1,7 @@
+export default function dashboard() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>dashboard</h2>
+    </main>
+  );
+}
