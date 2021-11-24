2d1c3e057454ee1e4d27e726aafd53a4a650a7b1
Donnie Walsh <donfather1000@gmail.com>
2021-11-24 06:08:54 +0000

    work on searchbar query logic

--- front-end-react/src/components/Home/VegetableCardList.tsx
+++ front-end-react/src/components/Home/VegetableCardList.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import './VegetableCardList.scss'
+import "./VegetableCardList.scss";
 
-import VegetableCard from './VegetableCard'
+import VegetableCard from "./VegetableCard";
 
+export default function VegetableCardList(props: any) {
+  const vegetables = props.vegetables;
+  console.log(vegetables)
+  const listVegetables = vegetables.map(() => {
+   return <VegetableCard />;
+  });
+  console.log(listVegetables)
+  return <ul className="VegetableCardList">{listVegetables}</ul>;
 
-export default function VegetableCardList(props:any) {
-  const { state } = props;
-  const vegetableArray = state.vegetables;
-  console.log(vegetableArray)
-  return (
-    <div className='VegetableCardList'>
-      <VegetableCard state={state}/>
-    </div>
-  );
 }
