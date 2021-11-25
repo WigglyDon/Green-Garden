0c115eece649f5f5012d16d85d0302ab855149e6
Edmund <eyu1@ualberta.ca>
2021-11-24 19:29:45 -0700

    Nav bar formatting and styling

--- front-end-react/src/components/Nav.tsx
+++ front-end-react/src/components/Nav.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Nav.scss";
 
 export default function Nav() {
   return (
-    <div>
-      <div>What to grow?</div>
-      <nav>
+    <div className="nav-layout">
+      <div className="nav-logo">
+        <Link to="/">What to grow?</Link>
+      </div>
+      <div className="nav-links">
         <Link to="/dashboard">My Garden</Link> | <Link to="/login">Login</Link>
-      </nav>
-      I'm a nav.
+      </div>
     </div>
   );
 }
