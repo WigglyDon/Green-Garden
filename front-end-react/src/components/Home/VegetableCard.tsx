6b2b85dc5aec643e523a61e0557c7c88e86ad283
Donnie <donfather1000@gmail.com>
2021-11-24 20:34:02 -0700

    import mui vegetablecard

--- front-end-react/src/components/Home/VegetableCard.tsx
+++ front-end-react/src/components/Home/VegetableCard.tsx
@@ -1,20 +1,52 @@
-import React from "react";
 import "./VegetableCard.scss";
 
+// export default function VegetableCard(props: any) {
+//   // console.log(props.vegetable)
+//   return (
+//     <div className="VegetableCard">
+//       {props.vegetable.name}
+//       <img
+//         className="VegetableCardImage"
+//         alt="veggie pic"
+//         src={props.vegetable.image_url}
+//       />
+//       <div>description: {props.vegetable.description}</div>
+//       <div>growing_days: {props.vegetable.growing_days}</div>
+//       <div>height: {props.vegetable.height}</div>
+//       <div>native_region: {props.vegetable.native_region}</div>
+//     </div>
+//   );
+// }
+//--------------------------------
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
 export default function VegetableCard(props: any) {
-  // console.log(props.vegetable)
   return (
-    <div className="VegetableCard">
-      {props.vegetable.name}
-      <img
-        className="VegetableCardImage"
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={props.vegetable.image_url}
         alt="veggie pic"
-        src={props.vegetable.image_url}
       />
-      <div>description: {props.vegetable.description}</div>
-      <div>growing_days: {props.vegetable.growing_days}</div>
-      <div>height: {props.vegetable.height}</div>
-      <div>native_region: {props.vegetable.native_region}</div>
-    </div>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.vegetable.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.vegetable.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 }
