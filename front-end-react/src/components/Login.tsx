130eba0bbf83bc26b9bfaf9038cc3b8b700e7804
Edmund <eyu1@ualberta.ca>
2021-11-24 19:44:15 -0700

    add basic login form

--- front-end-react/src/components/Login.tsx
+++ front-end-react/src/components/Login.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./Login.scss";
 
 export default function Login() {
-  return <div>I'asasgfsfsfafsafs</div>;
+  return (
+    <div className="login-layout">
+      <form>
+        <div className="container">
+          <label>
+            <b>Email</b>
+          </label>
+          <input type="text" placeholder="Enter Email" name="email" required />
+          <label>
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            required
+          />
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
